53cab56bce3b9a67146d6fef98e3481ecb1b2ece
tca <tcarland@gmail.com>
2021-01-10 14:57:57 -0800

    notice updates

--- include/FileNode.hpp
+++ include/FileNode.hpp
@@ -1,21 +1,19 @@
-/**
-  * @file   FileNode.hpp
-  * @author tcarland@gmail.com
+/** @file FileNode.hpp
   *
   * Copyright (c) 2009-2021 Timothy C. Arland <tcarland@gmail.com>
   *
-  * Volgen is free software: you can redistribute it and/or modify
+  * VolGen is free software: you can redistribute it and/or modify
   * it under the terms of the GNU General Public License as published by
   * the Free Software Foundation, either version 3 of the License, or
   * (at your option) any later version.
   *
-  * Volgen is distributed in the hope that it will be useful,
+  * VolGen is distributed in the hope that it will be useful,
   * but WITHOUT ANY WARRANTY; without even the implied warranty of
   * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   * GNU General Public License for more details.
   *
   * You should have received a copy of the GNU General Public License
-  * along with Volgen.  If not, see <https://www.gnu.org/licenses/>.
+  * along with VolGen.  If not, see <https://www.gnu.org/licenses/>.
   *
  **/
 #ifndef _VOLGEN_FILENODE_HPP_
